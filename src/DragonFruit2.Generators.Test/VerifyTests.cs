5802491b8b7792448817a74f3b0811e92ea2bc25
Kathleen Dollard <kathleen.a.dollard@gmail.com>
2026-01-20 11:51:24 -0500

    Cleanup and removed non-verify output tests

--- src/DragonFruit2.Generators.Test/VerifyTests.cs
+++ src/DragonFruit2.Generators.Test/VerifyTests.cs
@@ -12,7 +12,7 @@ public class VerifyTests
 
     [Theory]
     [ClassData(typeof(CommandInfoTheoryData))]
-    public Task CommandInfo(string desc, string argsSource, string consoleSource, CommandInfo expected, string _)
+    public Task CommandInfo(string desc, string argsSource, string consoleSource, CommandInfo expected)
     {
         var compilation = TestHelpers.GetCompilation(argsSource, consoleSource);
         var programTree = compilation.SyntaxTrees.Last();
@@ -25,7 +25,7 @@ public class VerifyTests
     }
 
     /// <summary>
-    /// This test returns the output for teh root args only. The integration test returns all of the files
+    /// This test returns the output for the root args only. If you want to confirm all the files, see the `GenerationTest`
     /// </summary>
     /// <param name="desc">Used as part of file name, so keep it short</param>
     /// <param name="_">Not used</param>
@@ -35,7 +35,7 @@ public class VerifyTests
     /// <returns></returns>
     [Theory]
     [ClassData(typeof(CommandInfoTheoryData))]
-    public Task CommandOutput(string desc, string _, string __, CommandInfo commandInfo, string ___)
+    public Task CommandOutput(string desc, string _, string __, CommandInfo commandInfo)
     {
         var actual = DragonFruit2Builder.GetSourceForCommandInfo(commandInfo);
 
@@ -45,17 +45,14 @@ public class VerifyTests
     }
 
 
+   //[Theory]
+   //[ClassData(typeof(CommandInfoTheoryData))]
+   // public Task Generation(string desc, string argsSource, string consoleSource, CommandInfo _, string __)
+   // {
+   //     var driver = VerifyHelpers.GetDriver(argsSource, consoleSource);
 
-
-    // The following test should ideally generate new code and compare to the code in the CommandOutput test 
-    //[Theory]
-    //[ClassData(typeof(CommandInfoTheoryData))]
-    //public Task Generation(string desc, string argsSource, string consoleSource, CommandInfo _, string __)
-    //{
-    //    var driver = VerifyHelpers.GetDriver(argsSource, consoleSource);
-
-    //    var verifySettings = new VerifySettings();
-    //    verifySettings.UseDirectory("Snapshots/GenOutput");
-    //    return Verify(driver, verifySettings).UseParameters(desc);
-    //}
+   //     var verifySettings = new VerifySettings();
+   //     verifySettings.UseDirectory($"Snapshots/GeneratedOutput/{desc}");
+   //     return Verify(driver, verifySettings).UseParameters(desc);
+   // }
 }
